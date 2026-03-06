526fc4691ab7304e37a2d337447b7befa1ebc17a
Ali Abdullahi <Abdul684@umn.edu>
2026-03-06 16:14:59 -0600

    Working on .h file and organizing files

--- kv_store.h
+++ kv_store.h
@@ -1,9 +1,18 @@
 #ifndef KV_STORE_H
 #define KV_STORE_H
+#include <pthread.h>
 
 #define TABLE_SIZE 500
+#define BUFSIZE 512
 
+typedef struct Node{
+    char *key;
+    char *val;
+    struct Node *next; //For collisions to hold more than one node at a specific array position
+} Node;   
 
+extern Node *kvStore[TABLE_SIZE];
+extern pthread_rwlock_t rwlock;
 
 
 #endif
