a1494b8bcd13650216d465b1a6de00361049fcda
Ali Abdullahi <ali.abdullahi1228@gmail.com>
2026-03-28 18:39:21 -0500

    Currently working on main.c to accept remote connections

--- main.c
+++ main.c
@@ -10,28 +10,35 @@ void handle_sigint(int signo) {
     keep_going = 0;
 }
 
+char *follower_ip= NULL;
+char *follower_port = NULL;
+
 int main(int argc, char **argv) {
     struct sigaction scheck;
     scheck.sa_handler= handle_sigint;
     scheck.sa_flags=0;
     sigemptyset(&scheck.sa_mask);
+
+
     if(sigaction(SIGINT,&scheck,NULL)==-1){
         perror("sigaction");
         return -1;
 
     }
   
-    if (argc < 2 || argc > 3) {
+    if (argc < 2 || argc == 3 || argc>4) {
         printf("Usage: %s <my_port> [follower_port]\n", argv[0]);
         return 1;
     }
 
-    if (argc == 3) {
-        follower_port = argv[2]; 
-        printf("Role: LEADER (Replicating to %s)\n", follower_port);
+    if (argc == 4) {
+        follower_ip=argv[2];
+        follower_port = argv[3]; 
+        printf("Role: LEADER (Replicating to %s:%s)\n", follower_port,follower_ip);
     } 
     else {
         follower_port = NULL;   
+        follower_ip= NULL;
         printf("Role: STANDALONE/FOLLOWER\n");
     }
 
